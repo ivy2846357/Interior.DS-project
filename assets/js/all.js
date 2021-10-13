edbf8195bdbb14d2f14dd02de34896364357bd78
ZAII <ivy2846357@icloud.com>
2021-10-13 12:32:35 +0800

    Update 2021-10-13T04:32:33.151Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,12 @@
 "use strict";
+
+//居家風格-swiper-電腦版
+var swiper = new Swiper(".swiper--room-style__lg", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
 //# sourceMappingURL=all.js.map
