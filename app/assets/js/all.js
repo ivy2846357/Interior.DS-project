f04984ece187e93427131359813cae07105f8283
ZAII <ivy2846357@icloud.com>
2021-10-13 12:32:13 +0800

    首頁-推薦設計公司版型完成

---
+++ app/assets/js/all.js
@@ -0,0 +1,9 @@
+//居家風格-swiper-電腦版
+var swiper = new Swiper(".swiper--room-style__lg", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
