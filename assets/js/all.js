e4fee58a97d13139a7a6e7b121250fdc219c91e1
ZAII <ivy2846357@icloud.com>
2021-10-13 17:02:55 +0800

    Update 2021-10-13T09:02:51.499Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,5 +8,53 @@ var swiper = new Swiper(".swiper--room-style__lg", {
     el: ".swiper-pagination",
     clickable: true
   }
+}); //屋主評價-swiper-平板/手機版
+
+var swiper = new Swiper(".swiper--recommend__sm", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+$(document).ready(function () {
+  // 廣告視窗顯示
+  $('.required--alert').slideDown(); // 點擊關閉廣告視窗
+
+  $('.required--alert__close').click(function (e) {
+    e.preventDefault();
+    $('.required--alert').remove();
+  }); //偵測滑鼠滾動顯示/隱藏
+
+  $(window).scroll(function (e) {
+    var scrollHeight = $(this).scrollTop(); // console.log(scrollHeight);
+    //顯示/隱藏廣告視窗
+
+    if (scrollHeight < 3970) {
+      $('.required--alert').slideDown();
+    } else {
+      $('.required--alert').slideUp();
+    } //顯示/隱藏右側icon列表
+
+
+    if (scrollHeight > 500) {
+      $('.nav--icon').fadeIn();
+    } else {
+      $('.nav--icon').fadeOut();
+    }
+  }); //點擊回到最上層
+
+  $('.scroll--top-btn').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
 });
 //# sourceMappingURL=all.js.map
