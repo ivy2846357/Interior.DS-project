478dde99c2c95728569949fa8925d423394e69c1
ZAII <ivy2846357@icloud.com>
2021-10-15 12:25:26 +0800

    Update 2021-10-15T04:25:24.040Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -31,6 +31,20 @@ var swiper = new Swiper(".swiper--business-cooperation", {
     el: ".swiper-pagination",
     clickable: true
   }
+}); //設計師作品-推薦設計師-swiper
+
+var swiper = new Swiper(".swiper--recommend-designer", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
 });
 $(document).ready(function () {
   // 廣告視窗顯示
