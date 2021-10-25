6396f9037ee5050064120612978b963144e2dc94
ZAII <ivy2846357@icloud.com>
2021-10-25 17:23:31 +0800

    Jquery/lightbox JS檔案修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -119,21 +119,6 @@ var swiper = new Swiper(".swiper--infinite-loop-slides-per-group__3", {
     },
 });
 
-//vanillajs-datepicker
-const datepickerMeasure = document.querySelector('input[name="datepickerMeasure"]');
-const datepickerMeasureElem = new Datepicker(datepickerMeasure, {
-    autohide: true,
-    nextArrow: '>',
-    prevArrow: '<'
-});
-
-const datepickerConstruction = document.querySelector('input[name="datepickerConstruction"]');
-const datepickerConstructionElem = new Datepicker(datepickerConstruction, {
-    autohide: true,
-    nextArrow: '>',
-    prevArrow: '<'
-});
-
 $(document).ready(function () {
     //下拉選單箭頭旋轉動畫
     $('.dropdown').click(function (e) {
@@ -200,7 +185,7 @@ $(document).ready(function () {
         }, 1000);
     })
 
-    //服務項目-客製家具
+    //服務項目-訂製傢俱
     $('.btn--furniture-making').click(function (e) {
         e.preventDefault();
         $('html,body').animate({
@@ -232,4 +217,53 @@ $(document).ready(function () {
         }
     })
 
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+
+
 })
+
+//AOS效果
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+//vanillajs-datepicker
+const datepickerMeasure = document.querySelector('input[name="datepickerMeasure"]');
+const datepickerMeasureElem = new Datepicker(datepickerMeasure, {
+    autohide: true,
+    nextArrow: '>',
+    prevArrow: '<'
+});
+
+const datepickerConstruction = document.querySelector('input[name="datepickerConstruction"]');
+const datepickerConstructionElem = new Datepicker(datepickerConstruction, {
+    autohide: true,
+    nextArrow: '>',
+    prevArrow: '<'
+});
