808570ca0bd067a527ff54898374c6eea2344ade
ZAII <ivy2846357@icloud.com>
2021-10-28 16:20:49 +0800

    調整jquery動畫時間

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
         e.preventDefault();
         $('html,body').stop().animate({
             scrollTop: 0
-        }, 1000);
+        }, 100);
     })
 
     //服務項目移動動畫
@@ -54,7 +54,7 @@ $(document).ready(function () {
         // console.log($(btnPosition).offset().top);
         $('html,body').stop().animate({
             scrollTop: $(btnPosition).offset().top - 100
-        }, 1000);
+        }, 100);
     })
 
     //設計師作品-分頁點擊效果
